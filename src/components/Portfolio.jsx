dabd668f8bc89d0e8529f3ef61435b9f129756b3
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-08-29 21:53:22 +0200

    +

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,58 +8,54 @@ function Portfolio() {
 
   return (
     <div className="Portfolio">
-        <div className='heading'>
-          <h4>
-            Portfolio
-          </h4> 
+
+      <section className='heading'>
+        <div className='title'>
+          <h4>Portfolio</h4>
           <div className='underline'></div>
         </div>
+      </section>
 
-        <div className='heading'>
-          What Services You Will Get From Me 
-        </div>
-        <div className='description'>
-          Here is a list over my last projects, so you can have a sneak peek and see.
-        </div>
-        <div className='underline'></div>
+      <section className='subheading'>
+        <div className='subtitle'>
+            What Services You Will Get From Me 
+          </div>
+          <div className='description'>
+            Here is a list over my last projects, so you can have a sneak peek and see.
+          </div>
+          <div className='underline'></div>
+      </section>
 
-        <div className='content-with-big'>
-          <div className='big-picture'>
-            <img src={avatar}></img>
-          </div>
-          <div className='small-pictures'>
-            <div className='small-picture1'>
-            <img src={avatar}></img>
-            </div>
-            <div className='small-picture2'>
-            <img src={avatar}></img>
-            </div>
-          </div>
+      <section className='projects'>
+        <div  id='image1' className='big-box'>
+          <img src={avatar} className='big-image'/>
         </div>
-        <div className='small-pictures'>
-          <div className='small-picture'>
-          <img src={avatar}></img>
-          </div>
-          <div className='small-picture'>
-          <img src={avatar}></img>
-          </div>
-          <div className='small-picture'>
-          <img src={avatar}></img>
-          </div>
+        <div id='image9' className='big-box'>
+          <img src={avatar} className='big-image'/>
         </div>
-        <div className='content-with-big content2'>
-          <div className='small-pictures'>
-              <div className='small-picture1'>
-              <img src={avatar}></img>
-              </div>
-              <div className='small-picture2'>
-              <img src={avatar}></img>
-              </div>
-            </div>
-            <div className='big-picture'>
-            <img src={avatar}></img>
-            </div>
+        <div id='image2' className='small-box'>
+          <img src={avatar} className='small-image'/>
         </div>
+        <div id='image3' className='small-box'>
+          <img src={avatar} className='small-image'/>
+        </div>
+        <div id='image4' className='small-box'>
+          <img src={avatar} className='small-image'/>
+        </div>
+        <div id='image5' className='small-box'>
+          <img src={avatar} className='small-image'/>
+        </div>
+        <div id='image6' className='small-box'>
+          <img src={avatar} className='small-image'/>
+        </div>
+        <div id='image7' className='small-box'>
+          <img src={avatar} className='small-image'/>
+        </div>
+        <div id='image8' className='small-box'>
+          <img src={avatar} className='small-image'/>
+        </div>
+      </section>
+  
     </div>
   );
 }
